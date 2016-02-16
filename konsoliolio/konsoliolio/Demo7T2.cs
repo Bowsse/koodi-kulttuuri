a9dabaf523425f8ae44d4f6e12949f09cf91d8ac
bowsse <jaakko.poyhonen@gmail.com>
2016-02-16 17:12:45 +0200

    teh

---
+++ konsoliolio/konsoliolio/Demo7T2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Rengas
+    {
+        public string Merkki { get; set; }
+
+        public string Tyyppi { get; set; }
+
+        public string Rengaskoko { get; set; }
+
+        public Rengas(string merkki, string tyyppi, string rengaskoko)
+        {
+            Merkki = merkki;
+            Tyyppi = tyyppi;
+            Rengaskoko = rengaskoko;
+        }
+
+        public override string ToString()
+        {
+            return "rengas: " + Merkki + ", " + Tyyppi + ", " + Rengaskoko;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int koko = 0;
+            bool loppu = false;
+            double reaali = 0;
+
+            StreamWriter intti = new System.IO.StreamWriter("T2Integers.txt");
+            intti.Close();
+            StreamWriter doubbel = new System.IO.StreamWriter("T2Doubles.txt");
+            doubbel.Close();
+
+            do
+            {
+                Console.WriteLine("Give a number : ");
+                a = Console.ReadLine();
+
+                try
+                {
+                    reaali = double.Parse(a);
+
+                    try
+                    {
+                        koko = int.Parse(a);
+
+                        File.AppendAllText("T2Integers.txt", koko + Environment.NewLine);
+
+                    }
+                    catch (System.FormatException)
+                    {
+                        File.AppendAllText("T2Doubles.txt", reaali + Environment.NewLine);
+                    }
+
+                }
+                catch (System.FormatException)
+                {
+                    loppu = true;
+                }
+
+                
+            } while (!loppu);
+
+        }
+
+
+    }
+}
+    
+
---
+++ konsoliolio/konsoliolio/Demo7T3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    class Ohjelma
+    {
+        public string Nimi { get; set; }
+
+        public string Kanava { get; set; }
+
+        public double Alku { get; set; }
+
+        public double Loppu { get; set; }
+
+        public string Info { get; set; }
+
+        public Ohjelma(string nimi, string kanava, double alku, double loppu, string info)
+        {
+            Nimi = nimi;
+            Kanava = kanava;
+            Alku = alku;
+            Loppu = loppu;
+            Info = info;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ohjelma> ohjelmat = new List<Ohjelma>();
+
+            ohjelmat.Add(new Ohjelma("Isojalan jäljillä", "FOX", 16.55, 17.25, "Rygge tietää mistä täydellisen aplarin löytää"));
+            ohjelmat.Add(new Ohjelma("Iasdasdä", "SUB", 11.55, 12.25, "NO KERRO JO"));
+
+
+            Stream writems = new FileStream("Ohjelmatlista.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+
+            IFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(writems, ohjelmat);
+
+            writems.Close();
+
+
+            Stream openStream = new FileStream("Ohjelmatlista.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            List<Ohjelma> readOhjelmat = (List<Ohjelma>)formatter.Deserialize(openStream);
+
+            openStream.Close();
+
+            foreach (Ohjelma o in readOhjelmat)
+            {
+                Console.WriteLine("Ohjelman nimi: {0}, kanava: {1}, alkaa: {2}, loppuu: {3}, info: {4}", o.Nimi, o.Kanava, o.Alku, o.Loppu, o.Info);
+            }
+
+        }
+
+
+    }
+}
+    
+
